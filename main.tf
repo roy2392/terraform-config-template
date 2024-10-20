f499ab8cf4e3692b427814a7a9b4c0842eae31ea
Roey Zalta <roey.zalta@gmail.com>
2024-10-20 18:14:22 +0300

    added server details as output

--- main.tf
+++ main.tf
@@ -141,6 +141,14 @@ output "server_public_ip" {
   value = aws_eip.one.public_ip
 }
 
+output "server_private_ip" {
+  value = aws_instance.web-server-instance.private_ip
+}
+
+output "server_id" {
+  value = aws_instance.web-server-instance.id
+}
+
 # 9. Create Ubuntu server and install/enable apache2
 
 resource "aws_instance" "web-server-instance" {
